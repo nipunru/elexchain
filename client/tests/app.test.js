c6cf22ad9be325d33a7170e9e6853415bc6e8769
nipungc <nipun@gardenconnect.com>
2023-10-24 11:21:00 +0530

    Test updates

--- client/tests/app.test.js
+++ client/tests/app.test.js
@@ -1,76 +1,95 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import App from "./App"; // Assuming the main app component is App.js
+import App from "../src/App";
 
 describe("ElexChain App", () => {
-  it("should allow the commissioner to login", () => {
-    const { getByLabelText, getByText } = render(<App />);
-    fireEvent.change(getByLabelText(/Email/i), {
+  const setup = () => {
+    const utils = render(<App />);
+    const getByEmail = utils.getByLabelText(/Email/i);
+    const getByPassword = utils.getByLabelText(/Password/i);
+    const getByLogin = utils.getByText(/Login/i);
+    return {
+      ...utils,
+      getByEmail,
+      getByPassword,
+      getByLogin,
+    };
+  };
+
+  it("handles commissioner login", () => {
+    const { getByEmail, getByPassword, getByLogin, getByText } = setup();
+
+    fireEvent.change(getByEmail, {
       target: { value: "commissioner@example.com" },
     });
-    fireEvent.change(getByLabelText(/Password/i), {
-      target: { value: "password123" },
-    });
-    fireEvent.click(getByText(/Login/i));
+    fireEvent.change(getByPassword, { target: { value: "password123" } });
+    fireEvent.click(getByLogin);
+
     expect(getByText(/Commissioner Dashboard/i)).toBeInTheDocument();
   });
 
-  it("should allow commissioner to create an election", () => {
-    const { getByText, getByLabelText } = render(<App />);
+  it("allows commissioner to create an election", () => {
+    const { getByText, getByLabelText } = setup();
+
     fireEvent.click(getByText(/Create Election/i));
     fireEvent.change(getByLabelText(/Election Name/i), {
       target: { value: "General Election 2024" },
     });
     fireEvent.click(getByText(/Submit/i));
+
     expect(getByText(/Election created successfully/i)).toBeInTheDocument();
   });
 
-  it("should allow commissioner to add candidates", () => {
-    const { getByText, getByLabelText } = render(<App />);
+  it("allows commissioner to add candidates", () => {
+    const { getByText, getByLabelText } = setup();
+
     fireEvent.click(getByText(/Add Candidate/i));
     fireEvent.change(getByLabelText(/Candidate Name/i), {
       target: { value: "John Doe" },
     });
     fireEvent.click(getByText(/Add/i));
+
     expect(getByText(/Candidate added successfully/i)).toBeInTheDocument();
   });
 
-  // Simulate an error case for adding candidates
-  it("should show an error for adding candidates if backend fails", () => {
-    const { getByText } = render(<App />);
+  it("shows an error when adding candidates if backend fails", () => {
+    const { getByText } = setup();
+
     fireEvent.click(getByText(/Add Candidates/i));
     expect(getByText(/Database write error/i)).toBeInTheDocument();
   });
 
-  it("should allow commissioner to start the election", () => {
-    const { getByText } = render(<App />);
+  it("allows commissioner to start and end the election", () => {
+    const { getByText } = setup();
+
     fireEvent.click(getByText(/Start Election/i));
     expect(getByText(/Election started successfully/i)).toBeInTheDocument();
-  });
 
-  it("should allow commissioner to end the election", () => {
-    const { getByText } = render(<App />);
     fireEvent.click(getByText(/End Election/i));
     expect(getByText(/Election ended successfully/i)).toBeInTheDocument();
   });
 
-  it("should allow admin to enable voting", () => {
-    const { getByText } = render(<App />);
+  it("allows admin to enable voting", () => {
+    const { getByText } = setup();
+
     fireEvent.click(getByText(/Enable Voting/i));
     expect(getByText(/Voting enabled successfully/i)).toBeInTheDocument();
   });
 
-  it("should allow voter to enter PIN and view candidates", () => {
-    const { getByText, getByLabelText } = render(<App />);
+  it("handles voter PIN entry and candidate view", () => {
+    const { getByText, getByLabelText } = setup();
+
     fireEvent.change(getByLabelText(/Enter PIN/i), {
       target: { value: "123456" },
     });
     fireEvent.click(getByText(/Submit/i));
+
     expect(getByText(/Candidate List/i)).toBeInTheDocument();
   });
 
-  it("should allow voter to cast a vote and show confirmation", () => {
-    const { getByText } = render(<App />);
+  it("confirms successful vote casting", () => {
+    const { getByText } = setup();
+
     fireEvent.click(getByText(/Vote for John Doe/i));
     expect(getByText(/Vote cast successfully/i)).toBeInTheDocument();
   });
--- server/README.md
+++ server/README.md
@@ -1,12 +1,14 @@
-# bb: User Service
+# User Service
 
 ## Description
-This API contains all user related endpoints and Authentication. 
 
+This API contains all user related endpoints and Authentication.
 
-##### Routing         : Express
-##### ODM Database    : Mongoose
-##### Authentication  : Passport, JWT
+##### Routing : Express
+
+##### ODM Database : Mongoose
+
+##### Authentication : Passport, JWT
 
 ## Installation
 
@@ -17,19 +19,22 @@ git clone {repo_name}
 ```
 
 #### Install Node Modules
+
 ```
 npm install
 ```
 
 #### Create .env File
-You will find a example.env file in the home directory. Paste the contents of that into a file named .env in the same directory. 
+
+You will find a example.env file in the home directory. Paste the contents of that into a file named .env in the same directory.
 Fill in the variables to fit your application
 
 #### API Documentation
+
 You can find api documentation from `<host>/<version>/api-docs` (EG: `http://localhost:4000/v1/api-docs`)
 
 To update the document run `npm run generate-swagger`
 
 #### CI/CD
-`develop`, `qa` and `uat` branches are configured with AWS CodePipeline
 
+`develop`, `qa` and `uat` branches are configured with AWS CodePipeline
